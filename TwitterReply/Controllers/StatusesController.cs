9a17f8e1b50dfedfa73aaf52f5bc91d5c8878507
Bruno Camilo Grillo <bcgrillo@gfi.es>
2019-04-09 17:14:27 +0200

    Primera prueba

---
+++ TwitterReply/Controllers/StatusesController.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace TwitterReply.Controllers
+{
+    public class StatusesController : ApiController
+    {
+        //api/statuses
+        public bool Post(JObject message)
+        {
+            return message["text"] != null;
+        }
+    }
+}
