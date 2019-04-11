be7969504dc2cd8db57cf29ee799d085dc518937
Bruno Camilo Grillo <bcgrillo@gfi.es>
2019-04-11 12:40:51 +0200

    Cambios de mensajes

--- TwitterReply/Controllers/StatusesController.cs
+++ TwitterReply/Controllers/StatusesController.cs
@@ -13,60 +13,48 @@ namespace TwitterReply.Controllers
         //api/statuses
         public async Task<string> Post(JObject tweetOject)
         {
-            try
+            string res = "";
+
+            var auth = new SingleUserAuthorizer
             {
-                string res = "";
-
-                var auth = new SingleUserAuthorizer
+                CredentialStore = new SingleUserInMemoryCredentialStore
                 {
-                    CredentialStore = new SingleUserInMemoryCredentialStore
-                    {
-                        ConsumerKey = tweetOject.Value<string>("consumerKey"),
-                        ConsumerSecret = tweetOject.Value<string>("consumerSecret"),
-                        AccessToken = tweetOject.Value<string>("accessToken"),
-                        AccessTokenSecret = tweetOject.Value<string>("accessTokenSecret")
-                    }
-                };
-
-                var score = tweetOject.Value<double>("score");
-
-                switch (score)
-                {
-                    case double n when (n <= 0.2):
-                        res = ":'(";
-                        break;
-                    case double n when (n > 0.2 && n<=0.4):
-                        res = ":'‑(";
-                        break;
-                    case double n when (n > 0.4 && n <= 0.6):
-                        res = ":‑|";
-                        break;
-                    case double n when (n > 0.6 && n <= 0.8):
-                        res = ":-]";
-                        break;
-                    case double n when (n > 0.8 && n <= 1):
-                        res = ":‑D";
-                        break;
-
+                    ConsumerKey = tweetOject.Value<string>("consumerKey"),
+                    ConsumerSecret = tweetOject.Value<string>("consumerSecret"),
+                    AccessToken = tweetOject.Value<string>("accessToken"),
+                    AccessTokenSecret = tweetOject.Value<string>("accessTokenSecret")
                 }
+            };
 
+            var score = tweetOject.Value<double>("score");
 
-                var username = tweetOject.Value<string>("userName");
-                var a = tweetOject.Value<ulong>("statusId");
-
-                var twitterCtx = new TwitterContext(auth);
-                var tweet = await twitterCtx.ReplyAsync(a,$"Los sentimientos de tu tweet @{username} son {res}");
-
-                return tweetOject.ToString();
-            }
-            catch (Exception e)
+            switch (score)
             {
+                case double n when (n <= 0.2):
+                    res = "Lamentamos mucho tu mensaje, en breve nos pondremos en contacto contigo... \u1F622";
+                    break;
+                case double n when (n > 0.2 && n <= 0.4):
+                    res = "Sentimo mucho leer esto. No siempre acertamos en todo como nos gustaría. \u1F61F";
+                    break;
+                case double n when (n > 0.4 && n <= 0.6):
+                    res = "Vaya, tomamos nota de tu mensaje. \u1F610";
+                    break;
+                case double n when (n > 0.6 && n <= 0.8):
+                    res = "¡Gracias por tu mensaje! Seguiremos mejorando. \u1F60A";
+                    break;
+                case double n when (n > 0.8 && n <= 1):
+                    res = "¡Wow! ¡Muchísimas gracias, nos alegra mucho leer esto! \u1F604";
+                    break;
 
-                throw ;
             }
+
+            var username = tweetOject.Value<string>("userName");
+            var statusId = tweetOject.Value<ulong>("statusId");
+
+            var twitterCtx = new TwitterContext(auth);
+            var tweet = await twitterCtx.ReplyAsync(statusId, $"Los sentimientos de tu tweet @{username} son {res}");
+
+            return tweet.ToString();
         }
-
-       
-
     }
 }
